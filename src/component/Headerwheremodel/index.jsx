59de9a587c104f080a1ac24d6743e7df51ac067f
krishvaghasiya705 <kvaghasiya705@gmail.com>
2024-10-02 11:00:34 +0530

    model created

--- src/component/Headerwheremodel/index.jsx
+++ src/component/Headerwheremodel/index.jsx
@@ -5,6 +5,9 @@ import Closeicon from "../../assets/svg/Closeicon";
 import Searchiconsc from "../../assets/svg/Searchiconsc";
 import Searchiconthrd from "../../assets/svg/searchiconthrd";
 
+import Dropdownicon from "../../assets/svg/dropdownicon";
+import { Modeldata } from './../modeliconsandata/index';
+
 function Headerwheremodel({ closeModal, isModalOpen }) {
   return (
     <div
@@ -29,32 +32,43 @@ function Headerwheremodel({ closeModal, isModalOpen }) {
       </div>
       <div className="header-where-model-body">
         <div
-          className={`header-where-model-body-main-content ${
-            isModalOpen ? "header-where-model-body-main-content-open" : ""
+          className={`header-where-model-body-main-content-main ${
+            isModalOpen ? "header-where-model-body-main-content-main-open" : ""
           }`}
         >
-          <div className="where-model-title-main">
-            <h1>Where to?</h1>
-          </div>
-          <div className="where-model-search-div-main">
-            <div className="where-model-search-div">
-              <div className="searcicon-input">
-                <Searchiconthrd />
-              </div>
-              <input type="search" placeholder="Search Destinations" />
+          <div className="header-where-model-body-main-content">
+            <div className="where-model-title-main">
+              <h1>Where to?</h1>
             </div>
-          </div>
-          <div className="where-model-search-main-content">
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam atque facere aut eaque a, praesentium voluptatem quam iste dolore ad aperiam placeat, error deleniti ipsa quasi. Dicta tenetur magni cumque?
-            Eum, sapiente, quisquam officia saepe eius in harum sit beatae quia perferendis optio exercitationem dicta eveniet quas est laborum vel quod numquam voluptates? Tempora quae laborum, dolorum dolorem iusto quas.
-            Quasi inventore dolor exercitationem excepturi qui accusantium eveniet quibusdam impedit quos iste fugiat obcaecati, vitae veniam vero repellendus ipsum sed praesentium beatae? Sit, corrupti ab? Quidem libero eaque quos rerum!
-            Fugit illum beatae voluptates asperiores commodi consequuntur repellendus necessitatibus ab nulla dolorem mollitia veniam sit error ducimus iste explicabo exercitationem ipsum nemo amet impedit, enim id libero quibusdam eum. Quia.
-            Commodi quisquam, optio officiis error quasi enim quae aliquid? Accusantium nostrum natus suscipit sint sapiente hic cum, libero id at asperiores aperiam quo? Culpa voluptatem assumenda pariatur laudantium, nisi veritatis!
-            Vero pariatur libero qui quam eos corporis alias necessitatibus maiores animi esse itaque architecto ratione aperiam molestias velit minus, debitis exercitationem doloribus repudiandae beatae dignissimos repellat. Magnam inventore natus non.
-            Minus et laboriosam sed quas. Voluptates alias recusandae, praesentium perspiciatis eos magnam ab mollitia aliquid illo tempore. Corrupti, fugit beatae quia quibusdam saepe voluptatem culpa tenetur praesentium, magni voluptas aliquam.
-            Natus et recusandae voluptates harum aperiam earum sunt possimus dolorem animi voluptatum dicta quibusdam voluptas, quam dolor laboriosam atque. Beatae nihil quae exercitationem vero quaerat! Quae perferendis unde laboriosam. Autem.
-            Est quis facilis at atque hic earum quisquam in, fugiat esse officiis sapiente aliquam modi unde ad dolorum beatae deserunt! Deleniti laboriosam nulla debitis animi? Debitis incidunt esse autem officia.
-            Cupiditate consectetur rem est eos minima veritatis cumque maiores cum enim voluptatibus non dolore exercitationem animi, alias illum quo esse soluta commodi dolor, nesciunt consequuntur maxime possimus ipsa minus? Voluptate?</p>
+            <div className="where-model-search-div-main">
+              <div className="where-model-search-div">
+                <div className="searcicon-input">
+                  <Searchiconthrd />
+                </div>
+                <input type="search" placeholder="Search Destinations" />
+              </div>
+            </div>
+            <div className="where-model-search-main-content">
+              <h2>Suggested destinations</h2>
+              {Modeldata.map(({ icon, title, label, altdata }, index) => (
+                <div
+                  className="where-model-search-main-content-box"
+                  key={index}
+                >
+                  <div className="where-model-search-main-content-box-icon">
+                    <img src={icon} alt={altdata} />
+                  </div>
+                  <div>
+                    <p>{title}</p>
+                    <span>{label}</span>
+                  </div>
+                </div>
+              ))}
+            </div>
+
+            <button type="button" className="dropdwon-button-main">
+              <Dropdownicon />
+            </button>
           </div>
         </div>
         <div
---
+++ src/component/modeliconsandata/index.jsx
@@ -0,0 +1,144 @@
+// <------------------- Icons ------------------->
+import oneicon from "../../assets/modelicons/1.png";
+import twoicon from "../../assets/modelicons/2.webp";
+import threeicon from "../../assets/modelicons/3.png";
+import fouricon from "../../assets/modelicons/4.webp";
+import fiveicon from "../../assets/modelicons/5.webp";
+import sixicon from "../../assets/modelicons/6.webp";
+import sevenicon from "../../assets/modelicons/7.webp";
+import eighticon from "../../assets/modelicons/8.png";
+import nineicon from "../../assets/modelicons/9.webp";
+import tenicon from "../../assets/modelicons/10.webp";
+import elevenicon from "../../assets/modelicons/11.webp";
+import twelweicon from "../../assets/modelicons/12.png";
+import thirteenicon from "../../assets/modelicons/13.webp";
+import fourteenicon from "../../assets/modelicons/14.webp";
+import fifthteenicon from "../../assets/modelicons/15.webp";
+import sixteenicon from "../../assets/modelicons/16.webp";
+import seventeenicon from "../../assets/modelicons/17.webp";
+import eighteenicon from "../../assets/modelicons/18.png";
+import nineteenicon from "../../assets/modelicons/19.webp";
+import twenteenicon from "../../assets/modelicons/20.webp";
+
+export const Modeldata = [
+  {
+    altdata: "icon",
+    icon: oneicon,
+    title: "Udaipur, Rajasthan",
+    label: "For its stunning architecture",
+  },
+  {
+    altdata: "icon",
+    icon: twoicon,
+    title: "North Goa, Goa",
+    label: "Popular beach destination",
+  },
+  {
+    altdata: "icon",
+    icon: threeicon,
+    title: "Mumbai, Maharashtra",
+    label: "For sights like Gateway of India",
+  },
+  {
+    altdata: "icon",
+    icon: fouricon,
+    title: "Jaipur, Rajasthan",
+    label: "For its stunning architecture",
+  },
+  {
+    altdata: "icon",
+    icon: fiveicon,
+    title: "Lonavala, Maharashtra",
+    label: "For nature lovers",
+  },
+  {
+    altdata: "icon",
+    icon: sixicon,
+    title: "New Delhi, Delhi",
+    label: "For its top-notch dining",
+  },
+  {
+    altdata: "icon",
+    icon: sevenicon,
+    title: "Pune City, Maharashtra",
+    label: "For sights like Dagdusheth Halwai Ganpati Temple",
+  },
+  {
+    altdata: "icon",
+    icon: eighticon,
+    title: "South Goa, Goa",
+    label: "Popular beach destination",
+  },
+  {
+    altdata: "icon",
+    icon: nineicon,
+    title: "Calangute, Goa",
+    label: "For its bustling nightlife",
+  },
+  {
+    altdata: "icon",
+    icon: tenicon,
+    title: "Manali, Himachal Pradesh",
+    label: "For nature lovers",
+  },
+  {
+    altdata: "icon",
+    icon: elevenicon,
+    title: "Rishikesh, Uttarakhand",
+    label: "A hidden gem",
+  },
+  {
+    altdata: "icon",
+    icon: twelweicon,
+    title: "Puducherry, Puducherry",
+    label: "For sights like Sri Aurobindo Ashram",
+  },
+  {
+    altdata: "icon",
+    icon: thirteenicon,
+    title: "Mount Abu, Rajasthan",
+    label: "Off the beaten path",
+  },
+  {
+    altdata: "icon",
+    icon: fourteenicon,
+    title: "Jodhpur, Rajasthan",
+    label: "A hidden gem",
+  },
+  {
+    altdata: "icon",
+    icon: fifthteenicon,
+    title: "Shimla, Himachal Pradesh",
+    label: "Great for winter getaways",
+  },
+  {
+    altdata: "icon",
+    icon: sixteenicon,
+    title: "Ujjain, Madhya Pradesh",
+    label: "Off the beaten path",
+  },
+  {
+    altdata: "icon",
+    icon: seventeenicon,
+    title: "Alibag, Maharashtra",
+    label: "A hidden gem",
+  },
+  {
+    altdata: "icon",
+    icon: eighteenicon,
+    title: "Greater Noida, Uttar Pradesh",
+    label: "Off the beaten path",
+  },
+  {
+    altdata: "icon",
+    icon: nineteenicon,
+    title: "Anjuna, Goa",
+    label: "For its bustling nightlife",
+  },
+  {
+    altdata: "icon",
+    icon: twenteenicon,
+    title: "Dehradun, Uttarakhand",
+    label: "A hidden gem",
+  },
+];
