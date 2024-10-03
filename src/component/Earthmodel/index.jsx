fef6feba876b1c5c1f0781a2a1dff8a8f5658363
krishvaghasiya705 <kvaghasiya705@gmail.com>
2024-10-03 10:50:58 +0530

    laguage model created

--- src/component/Earthmodel/index.jsx
+++ src/component/Earthmodel/index.jsx
@@ -1,11 +1,90 @@
 import React, { useState, useEffect } from "react";
 import "./Earthmodel.scss";
 import Crossicon from "../../assets/svg/Crossicon";
+import Translationicon from "../../assets/svg/Translationicon";
+import Trueicon from "../../assets/svg/Trueicon";
 
 function Earthmodel({ closeModal, isModalOpen }) {
   const [isVisible, setIsVisible] = useState(false);
   const [isClosing, setIsClosing] = useState(false);
   const [activeButtonIndex, setActiveButtonIndex] = useState(0);
+  const [isOn, setIsOn] = useState(true);
+  const [activeLanguageIndex, setActiveLanguageIndex] = useState(0);
+
+  const languages = [
+    { language: "English", region: "India" },
+    { language: "Azərbaycan dili", region: "Azərbaycan" },
+    { language: "Bahasa Indonesia", region: "Indonesia" },
+    { language: "Bosanski", region: "Bosna i Hercegovina" },
+    { language: "Català", region: "Espanya" },
+    { language: "Čeština", region: "Česká republika" },
+    { language: "Crnogorski", region: "Crna Gora" },
+    { language: "Dansk", region: "Danmark" },
+    { language: "Deutsch", region: "Deutschland" },
+    { language: "Deutsch", region: "Österreich" },
+    { language: "Deutsch", region: "Schweiz" },
+    { language: "Deutsch", region: "Luxemburg" },
+    { language: "Eesti", region: "Eesti" },
+    { language: "English", region: "Australia" },
+    { language: "English", region: "Canada" },
+    { language: "English", region: "Guyana" },
+    { language: "English", region: "Ireland" },
+    { language: "English", region: "New Zealand" },
+    { language: "English", region: "Singapore" },
+    { language: "English", region: "United Arab Emirates" },
+    { language: "Español", region: "Argentina" },
+    { language: "Español", region: "Belice" },
+    { language: "Español", region: "Bolivia" },
+    { language: "Español", region: "Chile" },
+    { language: "Español", region: "Colombia" },
+    { language: "Español", region: "Costa Rica" },
+    { language: "Español", region: "Ecuador" },
+    { language: "Español", region: "El Salvador" },
+    { language: "Español", region: "España" },
+    { language: "Español", region: "Estados Unidos" },
+    { language: "Español", region: "Guatemala" },
+    { language: "Español", region: "Honduras" },
+    { language: "Español", region: "Latinoamérica" },
+    { language: "Español", region: "México" },
+    { language: "Español", region: "Nicaragua" },
+    { language: "Español", region: "Panamá" },
+    { language: "Español", region: "Paraguay" },
+    { language: "Español", region: "Perú" },
+    { language: "Español", region: "Venezuela" },
+    { language: "Français", region: "Belgique" },
+    { language: "Français", region: "Canada" },
+    { language: "Français", region: "France" },
+    { language: "Français", region: "Suisse" },
+    { language: "Français", region: "Luxembourg" },
+    { language: "Gaeilge", region: "Éire" },
+    { language: "Hrvatski", region: "Hrvatska" },
+    { language: "isiXhosa", region: "eMzantsi Afrika" },
+    { language: "isiZulu", region: "iNingizimu Afrika" },
+    { language: "Íslenska", region: "Ísland" },
+    { language: "Italiano", region: "Italia" },
+    { language: "Italiano", region: "Svizzera" },
+    { language: "Kiswahili", region: "Āfrika" },
+    { language: "Latviešu", region: "Latvija" },
+    { language: "Lietuvių", region: "Lietuva" },
+    { language: "Magyar", region: "Magyarország" },
+    { language: "Malti", region: "Malta" },
+    { language: "Melayu", region: "Malaysia" },
+    { language: "Vlaams", region: "België" },
+    { language: "Nederlands", region: "Nederland" },
+    { language: "Norsk", region: "Norge" },
+    { language: "Polski", region: "Polska" },
+    { language: "Português", region: "Brasil" },
+    { language: "Português", region: "Portugal" },
+    { language: "Română", region: "România" },
+    { language: "Shqip", region: "Shqipëri" },
+    { language: "Slovenčina", region: "Slovensko" },
+    { language: "Slovenščina", region: "Slovenija" },
+    { language: "Srpski", region: "Srbija" },
+    { language: "Suomi", region: "Suomi" },
+    { language: "Svenska", region: "Sverige" },
+    { language: "Tagalog", region: "Pilipinas" },
+    { language: "Bahasa", region: "Indonesia" },
+  ];
 
   useEffect(() => {
     if (isModalOpen) {
@@ -19,7 +98,7 @@ function Earthmodel({ closeModal, isModalOpen }) {
       const timer = setTimeout(() => {
         setIsVisible(false);
         closeModal();
-      }, 300);
+      }, 500);
       return () => clearTimeout(timer);
     }
   }, [isClosing, closeModal]);
@@ -32,12 +111,22 @@ function Earthmodel({ closeModal, isModalOpen }) {
     setActiveButtonIndex(index);
   };
 
+  const toggleSwitch = () => {
+    setIsOn(!isOn);
+  };
+
+  const handleLanguageClick = (index) => {
+    setActiveLanguageIndex(index);
+  };
+
   if (!isVisible && !isModalOpen) return null;
 
   return (
-    <div className={`earth-model ${
+    <div
+      className={`earth-model ${
         isModalOpen && !isClosing ? "earth-model-open" : ""
-      } ${isClosing ? "earth-model-closing" : ""}`}>
+      } ${isClosing ? "earth-model-closing" : ""}`}
+    >
       <div
         className={`earth-model-main-background ${
           isModalOpen && !isClosing ? "earth-model-main-background-open" : ""
@@ -75,6 +164,76 @@ function Earthmodel({ closeModal, isModalOpen }) {
               >
                 Currency
               </button>
+            </div>
+            <div className="earth-model-inner-body-details">
+              <div className="earth-model-inner-body-details-first-box">
+                <div>
+                  <span>
+                    Translation
+                    <Translationicon />
+                  </span>
+                  <p>
+                    Automatically translate descriptions and reviews to English.
+                  </p>
+                </div>
+                <div>
+                  <div className="switch-container" onClick={toggleSwitch}>
+                    <div className={`switch ${isOn ? "switch-on" : ""}`}>
+                      <div className="switch-circle">
+                        {isOn && (
+                          <span className="icon-true">
+                            <Trueicon />
+                          </span>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="suggestion-box-main">
+              <h3>Suggested languages and regions</h3>
+              <div className="suggested-laguage-box-main">
+                <div className="suggested-laguage-box">
+                  <div className="suggested-laguage-box-inner">
+                    <p>English</p>
+                    <span>United States</span>
+                  </div>
+                </div>
+                <div className="suggested-laguage-box">
+                  <div className="suggested-laguage-box-inner">
+                    <p>English</p>
+                    <span>United Kingdom</span>
+                  </div>
+                </div>
+                <div className="suggested-laguage-box">
+                  <div className="suggested-laguage-box-inner">
+                    <p>हिन्दी</p>
+                    <span>भारत</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="choose-language-section-main">
+              <h4>Choose a language and region</h4>
+              <div className="choose-language-boxes-main">
+                {languages.map((languageItem, index) => (
+                  <div
+                    key={index}
+                    className={`laguage-box-main ${
+                      activeLanguageIndex === index
+                        ? "laguage-box-main-active"
+                        : ""
+                    }`}
+                    onClick={() => handleLanguageClick(index)}
+                  >
+                    <div className="laguage-box">
+                      <p>{languageItem.language}</p>
+                      <span>{languageItem.region}</span>
+                    </div>
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
